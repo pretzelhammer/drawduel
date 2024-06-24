3b37b599ce223e4b2fb89da07265bf9cd6d92409
Cindy Vu <cindyvudba12@gmail.com>
2024-06-24 13:18:34 -0400

    create component to build draw stuff on

--- src/frontend/game/GameApp.tsx
+++ src/frontend/game/GameApp.tsx
@@ -1,18 +1,12 @@
 import { type FunctionalComponent } from 'preact';
 import { ClientContextProvider } from 'src/frontend/game/ClientContextProvider.tsx';
-import { PlayerList } from 'src/frontend/game/components/PlayerList.tsx';
 import 'src/frontend/game/game-app.css';
-import { ChangeName } from 'src/frontend/game/components/ChangeName.tsx';
-import { IncreaseMyScore } from 'src/frontend/game/components/IncreaseMyScore.tsx';
+import { Main } from 'src/frontend/game/Main.tsx';
 
 export const GameApp: FunctionalComponent = () => {
 	return (
 		<ClientContextProvider>
-			<h1>draw duel 🎨⚔️</h1>
-			<h2>game page</h2>
-			<PlayerList />
-			<IncreaseMyScore />
-			<ChangeName />
+			<Main />
 		</ClientContextProvider>
 	);
 };
