c674ab1bbff2cf778e19e7a57671cabee436ed6e
Cindy Vu <cindyvudba12@gmail.com>
2024-06-24 23:48:59 -0400

    more quality of life

--- src/frontend/game/components/DrawStage.tsx
+++ src/frontend/game/components/DrawStage.tsx
@@ -99,6 +99,7 @@ export const DrawStage: FunctionalComponent = () => {
 							onClick={() =>
 								setBrushSettings({
 									...brushSettings,
+									tool: Tool.Pencil,
 									size,
 								})
 							}
