7356d18a276b100407809ca6e2c4dd124646f703
Cindy Vu <cindyvudba12@gmail.com>
2024-06-26 13:39:26 -0400

    canvas supports draw and preview

--- src/frontend/components/Canvas.tsx
+++ src/frontend/components/Canvas.tsx
@@ -2,6 +2,7 @@ import { type FunctionalComponent, type JSX } from 'preact';
 import { useEffect, useRef, useState } from 'preact/hooks';
 import classes from 'src/frontend/components/Canvas.module.css';
 import { ForwardedRef, forwardRef } from 'preact/compat';
+import { groupBy } from 'lodash-es';
 
 export interface BrushSettings {
 	readonly color: string;
@@ -13,26 +14,65 @@ export interface CanvasDimensions {
 	readonly height: number;
 }
 
-export interface CanvasProps {
+export enum CanvasMode {
+	Draw = 'CANVAS_MODE_DRAW',
+	Preview = 'CANVAS_MODE_PREVIEW',
+}
+
+export interface DrawData {
 	readonly brushSettings: BrushSettings;
 	readonly canvasDimensions: CanvasDimensions;
+	readonly start: {
+		readonly x: number;
+		readonly y: number;
+	};
+	readonly end: {
+		readonly x: number;
+		readonly y: number;
+	};
+}
+
+export interface DrawMode {
+	readonly name: CanvasMode.Draw;
+	readonly brushSettings: BrushSettings;
+	readonly canvasDimensions: CanvasDimensions;
+	readonly onDraw: (drawData: DrawData) => void;
+}
+
+export interface PreviewMode {
+	readonly name: CanvasMode.Preview;
+	readonly drawData: DrawData;
+}
+
+export interface CanvasProps {
+	readonly mode: DrawMode | PreviewMode;
 }
 
 export const Canvas: FunctionalComponent<CanvasProps> = forwardRef<HTMLCanvasElement, CanvasProps>(
-	({ brushSettings, canvasDimensions }, parentRef) => {
+	({ mode }, parentRef) => {
 		if (typeof parentRef === 'function') {
 			throw new Error(`Only refs that are created with createRef or useRef are supported`);
 		}
 
 		const localRef = useRef<HTMLCanvasElement | null>(null);
 		const canvasRef = parentRef ?? localRef;
-
-		useEffect(() => {
-			let coord = {
+		const [drawing, setDrawing] = useState(false);
+		const drawingRef = useRef(drawing);
+		drawingRef.current = drawing;
+		const [coords, setCoords] = useState({
+			start: {
 				x: 0,
 				y: 0,
-			};
+			},
+			end: {
+				x: 0,
+				y: 0,
+			},
+		});
+		const coordsRef = useRef(coords);
+		coordsRef.current = coords;
 
+		useEffect(() => {
 			const canvas = canvasRef.current;
 			if (!canvas) {
 				throw new Error('Could not get canvas');
@@ -43,45 +83,81 @@ export const Canvas: FunctionalComponent<CanvasProps> = forwardRef<HTMLCanvasEle
 				throw new Error('Could not get 2d context');
 			}
 
-			const start = (event: MouseEvent) => {
-				canvas.addEventListener('mousemove', draw);
-				reposition(event);
-			};
-
-			const stop = () => {
-				canvas.removeEventListener('mousemove', draw);
-			};
-
-			const reposition = (event: MouseEvent) => {
-				const rect = canvas.getBoundingClientRect();
-				const x = event.clientX - rect.left;
-				const y = event.clientY - rect.top;
-				coord = {
-					x: event.clientX - rect.left,
-					y: event.clientY - rect.top,
+			if (mode.name === CanvasMode.Draw) {
+				const reposition = (event: MouseEvent) => {
+					const rect = canvas.getBoundingClientRect();
+					const coord = coordsRef.current.end;
+					setCoords({
+						start: { x: coord.x, y: coord.y },
+						end: {
+							x: event.clientX - rect.left,
+							y: event.clientY - rect.top,
+						},
+					});
 				};
-			};
 
-			const draw = (event: MouseEvent) => {
-				const { color, size } = brushSettings;
+				const draw = (event: MouseEvent) => {
+					if (drawingRef.current) {
+						const { color, size } = mode.brushSettings;
+						context.beginPath();
+						context.strokeStyle = color;
+						context.lineWidth = size;
+						context.lineCap = 'round';
+						reposition(event);
+						context.moveTo(coordsRef.current.start.x, coordsRef.current.start.y);
+						context.lineTo(coordsRef.current.end.x, coordsRef.current.end.y);
+						context.stroke();
+						mode.onDraw({
+							brushSettings: mode.brushSettings,
+							canvasDimensions: mode.canvasDimensions,
+							...coordsRef.current,
+						});
+					}
+				};
+
+				const start = (event: MouseEvent) => {
+					const rect = canvas.getBoundingClientRect();
+					// set inital coords
+					setCoords({
+						start: {
+							x: event.clientX - rect.left,
+							y: event.clientY - rect.top,
+						},
+						end: {
+							x: event.clientX - rect.left,
+							y: event.clientY - rect.top,
+						},
+					});
+					setDrawing(true);
+				};
+
+				const stop = () => {
+					setDrawing(false);
+				};
+
+				canvas.addEventListener('mousemove', draw);
+				canvas.addEventListener('mousedown', start);
+				canvas.addEventListener('mouseup', stop);
+
+				return () => {
+					canvas.removeEventListener('mousedown', start);
+					canvas.removeEventListener('mouseup', stop);
+					canvas.removeEventListener('mousemove', draw);
+				};
+			} else if (mode.name === CanvasMode.Preview) {
+				const { color, size } = mode.drawData.brushSettings;
+				const { start, end } = mode.drawData;
 				context.beginPath();
 				context.strokeStyle = color;
 				context.lineWidth = size;
 				context.lineCap = 'round';
-				context.moveTo(coord.x, coord.y);
-				reposition(event);
-				context.lineTo(coord.x, coord.y);
+				context.moveTo(start.x, start.y);
+				context.lineTo(end.x, end.y);
 				context.stroke();
-			};
+			}
+		}, [mode]);
 
-			canvas.addEventListener('mousedown', start);
-			canvas.addEventListener('mouseup', stop);
-
-			return () => {
-				canvas.removeEventListener('mousedown', start);
-				canvas.removeEventListener('mouseup', stop);
-			};
-		}, [brushSettings]);
+		const canvasDimensions = mode.name === CanvasMode.Draw ? mode.canvasDimensions : mode.drawData.canvasDimensions;
 
 		return (
 			<canvas
